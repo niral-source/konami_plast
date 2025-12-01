ea1fef296fdf4e7d43f6fdef29be76c7d291273b
Niral <niral12598@gmail.com>
2025-12-01 20:00:05 +0530

    Merge remote master into local master

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -153,7 +153,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <h2 class="text-2xl font-bold text-gray-900 mb-6">Our Location</h2>
             <div class="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden">                
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3483.280948492355!2d72.44648677510045!3d23.293891778984484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c24932e11b94d%3A0x73b2a9a09dae6ebc!2sBright%20Tubular%20Components!5e1!3m2!1sen!2sin!4v1764596356252!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-lg shadow-lg"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27866.252357794107!2d72.4284926347656!3d23.29386900000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c250003b50d71%3A0x87ab0f9b119888b2!2sKonami%20Plast%20Engineering!5e1!3m2!1sen!2sin!4v1764599126830!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-lg shadow-lg"></iframe>
             </div>
         </div>
     </div>
